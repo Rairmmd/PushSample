92980f5dc5836db671ef09e9c1f299221086bbca
Rair <645207307@qq.com>
2017-11-03 17:13:41 +0800

    Initial commit

---
+++ BPushDemo/src/main/java/com/rairmmd/push/App.java
@@ -0,0 +1,22 @@
+package com.rairmmd.push;
+
+import android.app.Application;
+
+import com.baidu.android.pushservice.PushConstants;
+import com.baidu.android.pushservice.PushManager;
+
+/**
+ * @author Rair
+ * @date 2017/11/3
+ * <p>
+ * desc:
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PushManager.startWork(this, PushConstants.LOGIN_TYPE_API_KEY, "GwDx95VsGFthzGTWGG34xXfaQeusBpLj");
+    }
+}
---
+++ JPushDemo/src/main/java/com/rair/push/App.java
@@ -0,0 +1,21 @@
+package com.rairmmd.push;
+
+import android.app.Application;
+
+import cn.jpush.android.api.JPushInterface;
+
+/**
+ * @author Rair
+ * @date 2017/11/3
+ * <p>
+ * desc:
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        JPushInterface.init(this);
+    }
+}
